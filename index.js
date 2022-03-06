c3cfef6ea349011cb01c43c5eda4ebcf5d3dac3c
ManishChahar7523 <manishchahar@myanatomy.in>
2022-03-07 00:40:05 +0530

    port

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ mongoose.connect("mongodb+srv://manish:manish123@cluster0.3ucdb.mongodb.net/byju
 });
 
 // listening to 7000
-app.listen(7000, function () {
+app.listen(process.env.PORT || 7000, function () {
   console.log("listening to port : 7000");
 });
 
